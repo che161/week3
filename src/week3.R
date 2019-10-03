b363aa157ab2452ed2eef21c3784588a3c2ffc7c
Chen <che161@csiro.au>
2019-10-03 16:30:08 +1000

    week3 challenge 13

--- src/week3.R
+++ src/week3.R
@@ -61,15 +61,114 @@ gapminder_mean <- gapminder %>%
   summarise(mean_lifeexp = mean(lifeExp))
 gapminder_mean
 
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point(alpha = 0.3) +
+  scale_x_log10() +
+  geom_smooth(method = "lm",size = 1.5)
+
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point(aes(color = continent),alpha = 0.3) +
+  scale_x_log10() +
+  scale_colour_manual(values = c("red", "green", "purple", "blue", "yellow"))
+
+
+ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) +
+  geom_point()
+
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, color = continent)) +
+  geom_point(alpha = 0.5) + 
+  scale_x_log10() +
+  scale_colour_manual(values = c("red", "green", "purple", "blue", "yellow"))+
+  geom_smooth(method = "lm", colour = "red",size = 1.5) 
+
+
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp)) +
+  geom_point(aes(color = continent), alpha = 0.5) + 
+  scale_x_log10() +
+  geom_smooth(method = "lm", colour = "red",size = 1.5)
+
+a_countries <- filter(gapminder, str_starts(country, "A"))
+
+ggplot(data = a_countries, aes(x = year, y = lifeExp, color = continent)) +
+  geom_line() + 
+  facet_wrap( ~ country)
+
+ggplot(
+  data = gapminder, 
+  mapping = aes(x = gdpPercap, y = lifeExp, colour = continent, size = pop)
+) +
+  geom_point() +
+  scale_x_log10()+ 
+  facet_wrap( ~ year)
+
+ggplot(
+  data = gapminder, 
+  mapping = aes(x = gdpPercap, y = lifeExp, colour = continent, size = pop)
+) +
+  geom_point() +
+  scale_x_log10()+ 
+  facet_wrap( ~ year)
+
+b_countries <- group_by(gapminder, str_starts(country, "A"))
+
+ggplot(data = b_countries, aes(x = pop, y = lifeExp, color = continent)) +
+  geom_point() +
+  scale_x_log10()+ 
+  facet_wrap( ~ year)
+
+ggplot(
+  data = gapminder_1977, 
+  mapping = aes(x = gdpPercap, y = lifeExp, colour = continent, size = pop, label = country)
+) +
+  geom_point(alpha = 0.5, shape = "star") +
+  scale_x_log10() +
+  geom_text()
+
+gapminder_rich <- filter(gapminder_1977, gdpPercap >= 20000)
+
+ggplot(
+  data = gapminder_1977, 
+  mapping = aes(x = gdpPercap, y = lifeExp, colour = continent, size = pop, label = country)
+) +
+  geom_point(alpha = 0.5) +
+  scale_x_log10() +
+  geom_text(data = gapminder_rich)
+
+rough_plot <- ggplot(data = a_countries, aes(x = year, y = lifeExp, color = continent)) +
+  geom_line() + 
+  facet_wrap( ~ country)
+#rough_plot + scale_colour_brewer(paletter = "1")
+
+rough_plot + 
+  labs(title = "Figure 1",
+       x = "Year",
+       y = "Life_expectanccy",
+       colour = "Continent")+
+  theme(
+    panel.grid.major = element_blank(),
+    plot.title = element_text(size = 24),
+    axis.line = element_line(colour = "blue", size = 0.5)
+  )
+
+
 ?geom_point
 
 read_csv("data/OveatingDwellings.csv")
 overheat <- read_csv("data/OveatingDwellings.csv")
+overheat_ga <- gather(overheat, criteria, value , -State)
+overheat_ga
 # Using ggplot2
-ggplot(data = overheat)
+ggplot(data = overheat_ga)
 ?ggplot
 ggplot(
-  data = overheat, 
-  mapping = aes(x = State, y = Orig_criteria)
+  data = overheat_ga, 
+  mapping = aes(x = State, y = value, fill = criteria, label = value*100 )
 ) +
-  geom_col(width = 0.75,colour= "red")
+  geom_col(width = 0.7) + #  geom_col(fill = "green", width = 0.75) +
+  facet_wrap( ~ criteria) +
+  geom_text() +
+  labs(title = "Figure 1",
+       x = "State",
+       y = "%"
+)
+
